41492ac6180c0824ae0dc56ae4f62ee052acf698
Niels van der Weide <n.vanderweide@science.ru.nl>
2017-10-16 14:59:28 +0200

    Basic structure of project

---
+++ bags/finite_bags.v
@@ -0,0 +1,106 @@
+(** Definitions of the Kuratowski-finite sets via a HIT.
+    We do not need the computation rules in the development, so they are not present here.
+*)
+Require Import HoTT HitTactics.
+Require Export set_names.
+
+Module Export Bags.
+  Section Bags.
+    Private Inductive bag (A : Type) : Type :=
+    | E : bag A
+    | L : A -> bag A
+    | U : bag A -> bag A -> bag A.
+
+    Global Instance fset_empty : forall A, hasEmpty (bag A) := E.
+    Global Instance fset_singleton : forall A, hasSingleton (bag A) A := L.
+    Global Instance fset_union : forall A, hasUnion (bag A) := U.
+
+    Variable (A : Type).
+
+    Axiom assoc : forall (x y z : bag A),
+        x ∪ (y ∪ z) = (x ∪ y) ∪ z.
+
+    Axiom comm : forall (x y : bag A),
+        x ∪ y = y ∪ x.
+
+    Axiom nl : forall (x : bag A),
+        ∅ ∪ x = x.
+
+    Axiom nr : forall (x : bag A),
+        x ∪ ∅ = x.
+
+    Axiom trunc : IsHSet (bag A).
+
+  End Bags.
+
+  Arguments assoc {_} _ _ _.
+  Arguments comm {_} _ _.
+  Arguments nl {_} _.
+  Arguments nr {_} _.
+
+  Section bag_induction.
+    Variable (A : Type)
+             (P : bag A -> Type)
+             (H :  forall X : bag A, IsHSet (P X))
+             (eP : P ∅)
+             (lP : forall a: A, P {|a|})
+             (uP : forall (x y: bag A), P x -> P y -> P (x ∪ y))
+             (assocP : forall (x y z : bag A)
+                               (px: P x) (py: P y) (pz: P z),
+                  assoc x y z #
+                        (uP x (y ∪ z) px (uP y z py pz))
+                  =
+                  (uP (x ∪ y) z (uP x y px py) pz))
+             (commP : forall (x y: bag A) (px: P x) (py: P y),
+                  comm x y #
+                       uP x y px py = uP y x py px)
+             (nlP : forall (x : bag A) (px: P x),
+                  nl x # uP ∅ x eP px = px)
+             (nrP : forall (x : bag A) (px: P x),
+                  nr x # uP x ∅ px eP = px).
+
+    (* Induction principle *)
+    Fixpoint bag_ind
+             (x : bag A)
+             {struct x}
+      : P x
+      := (match x return _ -> _ -> _ -> _ -> _ -> P x with
+          | E => fun _ _ _ _ _ => eP
+          | L a => fun _ _ _ _ _ => lP a
+          | U y z => fun _ _ _ _ _ => uP y z (bag_ind y) (bag_ind z)
+          end) H assocP commP nlP nrP.
+  End bag_induction.
+
+  Section bag_recursion.
+    Variable (A : Type)
+             (P : Type)
+             (H: IsHSet P)
+             (e : P)
+             (l : A -> P)
+             (u : P -> P -> P)
+             (assocP : forall (x y z : P), u x (u y z) = u (u x y) z)
+             (commP : forall (x y : P), u x y = u y x)
+             (nlP : forall (x : P), u e x = x)
+             (nrP : forall (x : P), u x e = x).
+
+    (* Recursion princople *)
+    Definition bag_rec : bag A -> P.
+    Proof.
+      simple refine (bag_ind A _ _ _ _ _ _ _ _ _)
+      ; try (intros ; simple refine ((transport_const _ _) @ _)) ; cbn.
+      - apply e.
+      - apply l.
+      - intros x y ; apply u.
+      - apply assocP.
+      - apply commP.
+      - apply nlP.
+      - apply nrP.
+    Defined.
+  End bag_recursion.
+
+  Global Instance bag_recursion A : HitRecursion (bag A) :=
+    {
+      indTy := _; recTy := _;
+      H_inductor := bag_ind A; H_recursor := bag_rec A
+    }.
+End Bags.
---
+++ interfaces/set_names.v
@@ -0,0 +1,60 @@
+(** Classes for set operations, so they can be overloaded. *)
+Require Import HoTT.
+
+(** The operations on sets for which we add names. *)
+Section structure.
+  Variable (T A : Type).
+  
+  Class hasMembership : Type :=
+    member : A -> T -> hProp.
+
+  Class hasMembership_decidable : Type :=
+    member_dec : A -> T -> Bool.
+
+  Class hasSubset : Type :=
+    subset : T -> T -> hProp.
+
+  Class hasSubset_decidable : Type :=
+    subset_dec : T -> T -> Bool.
+
+  Class hasEmpty : Type :=
+    empty : T.
+
+  Class hasSingleton : Type :=
+    singleton : A -> T.
+  
+  Class hasUnion : Type :=
+    union : T -> T -> T.
+
+  Class hasIntersection : Type :=
+    intersection : T -> T -> T.
+
+  Class hasComprehension : Type :=
+    filter : (A -> Bool) -> T -> T.
+End structure.
+
+Arguments member {_} {_} {_} _ _.
+Arguments subset {_} {_} _ _.
+Arguments member_dec {_} {_} {_} _ _.
+Arguments subset_dec {_} {_} _ _.
+Arguments empty {_} {_}.
+Arguments singleton {_} {_} {_} _.
+Arguments union {_} {_} _ _.
+Arguments intersection {_} {_} _ _.
+Arguments filter {_} {_} {_} _ _.
+
+Notation "∅" := empty.
+Notation "{| x |}" :=  (singleton x).
+Infix "∪" := union (at level 8, right associativity).
+Notation "(∪)" := union (only parsing).
+Notation "( X ∪)" := (union X) (only parsing).
+Notation "( ∪ Y )" := (fun X => X ∪ Y) (only parsing).
+Infix "∩" := intersection (at level 8, right associativity).
+Notation "( ∩ )" := intersection (only parsing).
+Notation "( X ∩ )" := (intersection X) (only parsing).
+Notation "( ∩ Y )" := (fun X => X ∩ Y) (only parsing).
+Notation "{| X & ϕ |}" := (filter ϕ X).
+Infix "∈" := member (at level 9, right associativity).
+Infix  "⊆" := subset (at level 10, right associativity).
+Infix "∈_d" := member_dec (at level 9, right associativity).
+Infix  "⊆_d" := subset_dec (at level 10, right associativity).
